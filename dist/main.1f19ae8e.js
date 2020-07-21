88fb0c2667428012997775a52dea3478e41c69e2
oldDong <2232890213@qq.com>
2020-07-21 20:21:38 +0800

    跳跃的代码

---
+++ dist/main.1f19ae8e.js
@@ -0,0 +1,359 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  var error;
+  for (var i = 0; i < entry.length; i++) {
+    try {
+      newRequire(entry[i]);
+    } catch (e) {
+      // Save first error but execute all entries
+      if (!error) {
+        error = e;
+      }
+    }
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  parcelRequire = newRequire;
+
+  if (error) {
+    // throw error from earlier, _after updating parcelRequire_
+    throw error;
+  }
+
+  return newRequire;
+})({"main.js":[function(require,module,exports) {
+var html = document.querySelector("#html");
+var css = document.querySelector("#style");
+var str = "\n /*\u5927\u5BB6\u597D\uFF0C\u6211\u662F\u4E00\u4E2A\u524D\u7AEF\u7A0B\u5E8F\u733F\n \u63A5\u4E0B\u6765\uFF0C\n \u6211\u8981\u5F00\u59CB\u753B\u4E00\u4E2A\u592A\u6781\u4E86\n \u9996\u5148\u51C6\u5907\u4E00\u4E2Adiv\n */\n    #div1 {\n        border: 1px solid red;\n        width: 200px;\n        height: 200px;\n    }\n /* \u628Adiv\u53D8\u6210\u4E00\u4E2A\u5706 \n \u80CC\u666F\u6E10\u53D8\n \u5DE6\u53F3\u9634\u9633\n */\n    #div1 {\n        border-radius: 50%;\n        box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);\n        border: none;\n        background: linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 1) 49%,\n                rgba(0, 0, 0, 1) 50%, rgba(0, 0, 0, 1) 100%);\n    }\n /*\u5F00\u59CB\u5236\u4F5C\u9634\u9633\u773C\n */\n    #div1::before {\n        content: '';\n        display: block;\n        background-color: #fff;\n        width: 100px;\n        height: 100px;\n        position: relative;\n        top: 0;\n        left: 50%;\n        transform: translateX(-50%);\n        border-radius: 50%;\n        background: radial-gradient(circle, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 1) 25%, rgba(255, 255, 255, 1) 25%, rgba(255, 255, 255, 1) 100%);\n    }\n\n    #div1::after {\n        display: block;\n        content: '';\n        background-color: #000;\n        width: 100px;\n        height: 100px;\n        position: relative;\n        top: 0;\n        left: 50%;\n        transform: translateX(-50%);\n        border-radius: 50%;\n        background: radial-gradient(circle, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 1) 25%, rgba(0, 0, 0, 1) 25%, rgba(0, 0, 0, 1) 100%);\n    }\n    /*\u8BA9\u592A\u6781\u65CB\u8F6C\u8D77\u6765*/\n    #div1{\n        animation: xuanzhuan 3s linear infinite;\n    }\n";
+var n = 0; // str2用于缓存
+
+var str2 = ""; //使用递归调用，可以随意控制停止  
+
+var step = function step() {
+  setTimeout(function () {
+    if (str[n] === '\n') {
+      // 如果是回车就换行
+      str2 += "<br>";
+    } else if (str[n] === ' ') {
+      //如果是空格
+      str2 += "&nbsp;";
+    } else {
+      //如果不是就继续打字
+      str2 += str[n];
+    }
+
+    html.innerHTML = str2;
+    css.innerHTML = str.substring(0, n);
+    window.scrollTo(0, 9999);
+    html.scrollTo(0, 99999);
+
+    if (n + 1 < str.length) {
+      n = n + 1;
+      step();
+    }
+  }, 20);
+};
+
+step();
+},{}],"C:/Users/Uath/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+var OldModule = module.bundle.Module;
+
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+  module.bundle.hotData = null;
+}
+
+module.bundle.Module = Module;
+var checkedAssets, assetsToAccept;
+var parent = module.bundle.parent;
+
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = "" || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "9686" + '/');
+
+  ws.onmessage = function (event) {
+    checkedAssets = {};
+    assetsToAccept = [];
+    var data = JSON.parse(event.data);
+
+    if (data.type === 'update') {
+      var handled = false;
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          var didAccept = hmrAcceptCheck(global.parcelRequire, asset.id);
+
+          if (didAccept) {
+            handled = true;
+          }
+        }
+      }); // Enable HMR for CSS by default.
+
+      handled = handled || data.assets.every(function (asset) {
+        return asset.type === 'css' && asset.generated.js;
+      });
+
+      if (handled) {
+        console.clear();
+        data.assets.forEach(function (asset) {
+          hmrApply(global.parcelRequire, asset);
+        });
+        assetsToAccept.forEach(function (v) {
+          hmrAcceptRun(v[0], v[1]);
+        });
+      } else if (location.reload) {
+        // `location` global exists in a web worker context but lacks `.reload()` function.
+        location.reload();
+      }
+    }
+
+    if (data.type === 'reload') {
+      ws.close();
+
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+      removeErrorOverlay();
+    }
+
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+      removeErrorOverlay();
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+
+  if (overlay) {
+    overlay.remove();
+  }
+}
+
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID; // html encode message and stack trace
+
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+  return overlay;
+}
+
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return [];
+  }
+
+  var parents = [];
+  var k, d, dep;
+
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+
+  return parents;
+}
+
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+
+function hmrAcceptCheck(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (!modules[id] && bundle.parent) {
+    return hmrAcceptCheck(bundle.parent, id);
+  }
+
+  if (checkedAssets[id]) {
+    return;
+  }
+
+  checkedAssets[id] = true;
+  var cached = bundle.cache[id];
+  assetsToAccept.push([bundle, id]);
+
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    return true;
+  }
+
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAcceptCheck(global.parcelRequire, id);
+  });
+}
+
+function hmrAcceptRun(bundle, id) {
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+
+  delete bundle.cache[id];
+  bundle(id);
+  cached = bundle.cache[id];
+
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+
+    return true;
+  }
+}
+},{}]},{},["C:/Users/Uath/AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js","main.js"], null)
+//# sourceMappingURL=/main.1f19ae8e.js.map
---
+++ src/main.js
@@ -0,0 +1,89 @@
+let html = document.querySelector("#html");
+let css = document.querySelector("#style");
+let str = `
+ /*大家好，我是一个前端程序猿
+ 接下来，
+ 我要开始画一个太极了
+ 首先准备一个div
+ */
+    #div1 {
+        border: 1px solid red;
+        width: 200px;
+        height: 200px;
+    }
+ /* 把div变成一个圆 
+ 背景渐变
+ 左右阴阳
+ */
+    #div1 {
+        border-radius: 50%;
+        box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+        border: none;
+        background: linear-gradient(90deg, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 1) 49%,
+                rgba(0, 0, 0, 1) 50%, rgba(0, 0, 0, 1) 100%);
+    }
+ /*开始制作阴阳眼
+ */
+    #div1::before {
+        content: '';
+        display: block;
+        background-color: #fff;
+        width: 100px;
+        height: 100px;
+        position: relative;
+        top: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        border-radius: 50%;
+        background: radial-gradient(circle, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 1) 25%, rgba(255, 255, 255, 1) 25%, rgba(255, 255, 255, 1) 100%);
+    }
+
+    #div1::after {
+        display: block;
+        content: '';
+        background-color: #000;
+        width: 100px;
+        height: 100px;
+        position: relative;
+        top: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        border-radius: 50%;
+        background: radial-gradient(circle, rgba(255, 255, 255, 1) 0%, rgba(255, 255, 255, 1) 25%, rgba(0, 0, 0, 1) 25%, rgba(0, 0, 0, 1) 100%);
+    }
+    /*让太极旋转起来*/
+    #div1{
+        animation: xuanzhuan 3s linear infinite;
+    }
+`;
+
+let n = 0;
+// str2用于缓存
+let str2 = "";
+
+//使用递归调用，可以随意控制停止  
+let step = () => {
+    setTimeout(() => {
+        if (str[n] === '\n') {
+            // 如果是回车就换行
+            str2 += "<br>";
+        } else if (str[n] === ' ') {
+            //如果是空格
+            str2 += "&nbsp;";
+        } else {
+            //如果不是就继续打字
+            str2 += str[n];
+        }
+        html.innerHTML = str2;
+        css.innerHTML = str.substring(0, n);
+        window.scrollTo(0, 9999);
+        html.scrollTo(0, 99999);
+        if (n + 1 < str.length) {
+            n = n + 1;
+
+            step();
+        }
+
+    }, 20)
+}
+step();
